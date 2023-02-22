720eb3b700db15d735d7cf0fc58c7c093af115f0
mofirojean <mofirojean@gmail.com>
2023-02-22 15:02:35 +0100

    implemented the priority queue

--- TypeScript/Data Structures/queue.ts
+++ TypeScript/Data Structures/queue.ts
@@ -36,6 +36,55 @@ class Queue<T> {
 }
 
 
+// Implementation of a priority queue
+class PriorityQueue<PriorityEntry> {
+    // create an empty unbounded
+    constructor(
+        private qList: PriorityEntry[]
+    ){}
+
+    // Returns true if the queue is emmpty
+    isEmpty(): boolean {
+        return this.qList.length == 0;
+    }
+
+    // returns the number of items in the queue
+    length(): number {
+        return this.qList.length;
+    }
+
+    // Adds a given item to the queue
+    enqueue(queueItem: PriorityEntry){
+        const entry: PriorityEntry = queueItem
+        this.qList.push(entry)
+    }
+
+    // Removes and returns the first item in the queue
+    dequeue() {
+        let highest:number = 0
+        for (let i = 0; i < this.qList.length; i++) {
+            if (this.qList[i].priority < highest){
+                highest = this.qList[i].priority
+            }
+        }
+        return this.qList.splice(highest, 1)
+    }
+}
+
+// Private class for storing the associaing queue items with thier priority
+interface PriorityEntry {
+    item: string,
+    priority: number
+}
+
+const list: PriorityEntry[] = [
+    {item: "hello", priority: 1},
+    {item: "master", priority: 2},
+    {item: "look", priority: 3},
+]
+
+console.log(list[0].priority)
+
 //  Driver Method
 let queue = new Queue()
 console.log(queue.isEmpty())
